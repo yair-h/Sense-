de6088bac2bae2cfdf1490abf062e83cc97808a2
Yair H <yairju1@gmail.com>
2017-07-04 21:32:15 +0300

    Add files via upload

---
+++ C++/lib/Sense.cpp
@@ -0,0 +1,71 @@
+#include "Sense.h"
+#include <Arduino.h>
+
+
+Sense::Sense(char *apiName) {
+  apiName_ = apiName;
+}
+
+Sense::~Sense() {}
+
+int Sense::begin(uint8_t baudRate) {
+  pinMode (debugRedLed,OUTPUT);pinMode (debugBlueLed,OUTPUT);
+  Serial.begin(baudRate);
+  while (!isConnected) {
+    if (Serial.available() > 0)  ack = Serial.read();
+    if (ack == '1') {
+      Serial.flush();
+      ack = NULL;
+      int answer = sendWhoAmI();
+      if (answer == 1) isConnected = true;
+      if (answer == 0) return 0;
+      if (answer == -1) return -1;
+    }
+  }
+  return true;
+}
+
+int Sense::sendWhoAmI() {
+  uint8_t counter = 0;
+  while (counter <= repeats) {
+    if (Serial.available() == 0) Serial.println(apiName_);
+    if (Serial.available() > 0) ack = Serial.read();
+    switch (ack) {
+      case ('1'): // we've got OK msg from app.
+        isIdentified = true;
+        return 1;
+        break;
+      case ('2'): // we've got a unrecognizable hardware ack message.
+        isIdentified = false;
+        return -1;
+        break;
+      default:
+        break;
+    }
+    delay(500);
+    counter++;
+  }
+  return 0;
+}
+
+void Sense::error(uint8_t errorNum) {
+  switch (errorNum) {
+    case (2):
+      while (true) {
+        digitalWrite(debugRedLed, HIGH);
+        delay(1000);
+        digitalWrite(debugRedLed, LOW);
+        delay(1000);
+      }
+      break;
+    case (1):
+      break;
+  }
+}
+
+bool Sense::begin(){
+  
+}
+
+
+
---
+++ C++/sensors_code/sensors_code.ino
@@ -0,0 +1,27 @@
+// **********************************************************************//
+/* list of sensors
+  temperature Sensor - SHT31 - https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/2_Humidity_Sensors/Sensirion_Humidity_Sensors_SHT3x_Datasheet_digital.pdf
+  humidity Sensor - SHT31
+  uv Sensor - ML8511 - https://cdn.sparkfun.com/datasheets/Sensors/LightImaging/ML8511_3-8-13.pdf
+  proximity Sensor - VL6180X - http://www.st.com/content/ccc/resource/technical/document/datasheet/c4/11/28/86/e6/26/44/b3/DM00112632.pdf/files/DM00112632.pdf/jcr:content/translations/en.DM00112632.pdf
+  barometric Sensor - BMP180 - https://cdn-shop.adafruit.com/datasheets/BST-BMP180-DS000-09.pdf
+  heartRate Sensor -  MAX30102 https://datasheets.maximintegrated.com/en/ds/MAX30102.pdf
+  alcohol Sensor -  MQ-3 https://www.sparkfun.com/datasheets/Sensors/MQ-3.pdf
+  soil Moisture Sensor
+  EC Sensor
+  
+*/
+// **********************************************************************//
+#include <Sense.h>
+
+Sense sense("sense_s1"); // Here we define the name of this device.
+
+void setup() {
+  sense.begin(9600);
+  if (!sense.isIdentified) sense.error(2); // this error tells that the app didn't recognize the device.
+  delay(1000);
+}
+
+void loop() {
+  if (!sense.sensorSelection()); sense.error(3);
+}
